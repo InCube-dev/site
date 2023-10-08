7bb6c82df95ce6489e984782c7194f414cbc42f4
VkProfi <wasechcyn@gmail.com>
2023-10-08 21:23:31 +0300

    сделанны страницы и изменён views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 from app import app
 from flask import render_template
-import auth
 
 @app.route('/') # Главная
 def index():
@@ -8,28 +7,38 @@ def index():
 
 @app.route('/discords') # дискорд сервера
 def index():
-    return render_template("discords.html")
+    return render_template("discords.html",title="Дисорды")
 
 @app.route('/options') # настройки
 def index():
-    return render_template("options.html")
+    return render_template("options.html",title="Настройки")
 
 @app.route('/profile') # профиль
 def index():
-    return render_template("profiles.html")
+    return render_template("profiles.html",title="Профиль")
 
 @app.route('/wiki') # вики
 def index():
-    return render_template("wiki.html")
+    return render_template("wiki.html",title="Wiki")
+
+@app.route('/wiki/<string:player>') # вики игрока
+def index(player):
+    wiki = {
+        "nick":"",
+        "about":"",
+        "avatarURL":"",
+        "status":""
+    }
+    return render_template("wikiPlayer.html",wiki=wiki,title=wiki["nick"])
 
 @app.route('/content') # видео/стримы
 def index():
-    return render_template("contents.html")
+    return render_template("contents.html",title="Контент")
 
 @app.route('/buy') # покупка
 def index():
-    return render_template("buy.html")
+    return render_template("buy.html",title="Покупка")
 
 @app.route('/incubepass') # инкубпасс
 def index():
-    return render_template("pass.html")
+    return render_template("pass.html",title="InCube Pass")
